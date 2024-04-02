d2f8b55b97efe97266e5b009ff3990d4350fc45f
nhuanh9 <chopperlouis@gmail.com>
2024-04-03 01:06:51 +0700

    cmt1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
+let x = 20;
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>Hello C10 {x}</h1>
   );
 }
-
+// Hien 1 doi tuong
+// Hien 1 mang
+// Hien 1 mang doi tuong
 export default App;
